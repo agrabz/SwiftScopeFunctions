28f35327f40ad0c6558650daedbe8a5c5678b9d6
Akos Grabecz <akos.grabecz@gmail.com>
2025-04-01 14:25:45 +0200

    Create With.swift

---
+++ With.swift
@@ -0,0 +1,75 @@
+///
+/// Result builder to collect the keypaths and values in a type safe way.
+/// Parameters:
+/// - `T` The type of the object that will be updated. Inside the result builder it is used only for type inference.
+///
+@resultBuilder
+fileprivate enum WithBuilder<T> {
+    ///
+    /// Implemented for better type inference. Namely, without this function we wouldn't be able to use keypaths without their root type in the `builder` closure of the `with` function.
+    ///
+    static func buildExpression<each V>(
+        _ components: repeat (
+            WritableKeyPath<T, each V>,
+            each V
+        )
+    ) -> (repeat (WritableKeyPath<T, each V>, each V)) {
+        (repeat each components)
+    }
+
+    ///
+    /// Builder function to collect the keypaths and values.
+    ///
+    static func buildBlock<each V>(
+        _ components: repeat (
+            WritableKeyPath<T, each V>,
+            each V
+        )
+    ) -> (repeat (WritableKeyPath<T, each V>, each V)) {
+        (repeat each components)
+    }
+}
+
+///
+/// Example type used in the examples below.
+/// ```
+/// struct Person {
+///     var name: String?
+///     var age: Int?
+/// }
+/// ```
+///
+/// Usage 1# | Initalization, setting properties, assigning the returning instance.:
+/// ```
+/// let akos = with(Person()) {
+///     (\.name, "Akos")
+///     (\.age, 32)
+/// }
+/// ```
+///
+/// Usage 2# | Passing in an instance, setting properties, assigning the returning instance.:
+/// ```
+/// var akos = Person()
+/// akos = with(akos) {
+///    (\.name, "Akos")
+///    (\.age, 32)
+/// }
+/// ```
+///
+/// Usage 3# | Not assigning the returning instance:
+/// ``TODO: Add example``
+///
+@discardableResult
+@inline(__always)
+func with<T, each V>(
+    _ item: T,
+    @WithBuilder<T> _ builder: () -> (repeat (WritableKeyPath<T, each V>, each V))
+) -> T {
+    var itemCopy = item
+    let updates = builder()
+    for update in repeat each updates {
+        let (keyPath, value) = update
+        itemCopy[keyPath: keyPath] = value
+    }
+    return itemCopy
+}
